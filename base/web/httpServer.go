5aaa336e0c8a05fb319fc43cf6079b375bdf0ca2
linkz <zyc737347123@163.com>
2020-01-07 11:36:04 +0800

    add two web handle

--- base/web/httpServer.go
+++ base/web/httpServer.go
@@ -13,3 +13,25 @@ func (h Hello) ServeHTTP(
 	r *http.Request) {
 	fmt.Fprint(w, "Hello!")
 }
+
+// String is a string web handle
+type String string
+
+func (str String) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprint(w, str)
+}
+
+// Struct is a struct web handle
+type Struct struct {
+	Greeting string
+	Punct    string
+	Who      string
+}
+
+func (str Struct) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprintf(w, "%s%s%s\n", str.Who, str.Punct, str.Greeting)
+}
--- main.go
+++ main.go
@@ -8,9 +8,8 @@ import (
 )
 
 func main() {
-	var h web.Hello
-	err := http.ListenAndServe("localhost:5000", h)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// your http.Handle calls here
+	http.Handle("/string", web.String("I'm a link zhang."))
+	http.Handle("/struct", &web.Struct{Who: "Hello", Punct: ":", Greeting: "Gophers!"})
+	log.Fatal(http.ListenAndServe("localhost:5000", nil))
 }
