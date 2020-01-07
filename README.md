eb25de8d78b16bdc61cf5b1fc8eb5b6441067cfc
linkz <zyc737347123@163.com>
2020-01-07 19:12:10 +0800

    增加学习进度

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # goPractice
 go practice with GOPATH (old way)
+
+## 1. 目前进度（The go program language）
+- 第一章
+
+## 2. 待做习题（The go program language）
+
+- 练习1.3
--- main.go
+++ main.go
@@ -1,15 +1,16 @@
+// Echo1 prints its command-line arguments.
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"github.com/zyc737347123/goPractice/base/web"
+	"fmt"
+	"os"
 )
 
 func main() {
-	// your http.Handle calls here
-	http.Handle("/string", web.String("I'm a link zhang."))
-	http.Handle("/struct", &web.Struct{Who: "Hello", Punct: ":", Greeting: "Gophers!"})
-	log.Fatal(http.ListenAndServe("localhost:5000", nil))
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 }
