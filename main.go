04f503ec2a046b298546b36082ddf5d48211f823
linkz <zyc737347123@163.com>
2020-01-08 14:32:40 +0800

    add new funcs

--- main.go
+++ main.go
@@ -3,28 +3,12 @@
 package main
 
 import (
-	"bufio"
 	"os"
 
 	"github.com/zyc737347123/goPractice/base/funcs"
 )
 
-// LineInfo is a struct for record text line info
-type LineInfo struct {
-	name string
-	text string
-}
-
 func main() {
 	files := os.Args[1:]
 	funcs.CountFilesLines(files)
-}
-
-func countLines(f *os.File, counts map[LineInfo]int, fileName string) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		lineInfo := LineInfo{name: fileName, text: input.Text()}
-		counts[lineInfo]++
-	}
-	// NOTE: ignoring potential errors from input.Err()
 }
