4ff897b07ac5b3fea3da8c2eaae2a89bb9e33bab
linkz <zyc737347123@163.com>
2020-01-08 14:30:27 +0800

    add new funcs

---
+++ base/funcs/count.go
@@ -0,0 +1,45 @@
+package funcs
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// LineInfo is a struct for record text line info
+type LineInfo struct {
+	name string
+	text string
+}
+
+func countLines(f *os.File, counts map[LineInfo]int, fileName string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		lineInfo := LineInfo{name: fileName, text: input.Text()}
+		counts[lineInfo]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
+
+// CountFilesLines will count all files same line
+func CountFilesLines(files []string) {
+	counts := make(map[LineInfo]int)
+	if len(files) == 0 {
+		countLines(os.Stdin, counts, "stdin")
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts, arg)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%s: %d\t%s\n", line.name, n, line.text)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,16 +1,30 @@
-// Echo1 prints its command-line arguments.
+// Dup2 prints the count and text of lines that appear more than once
+// in the input.  It reads from stdin or from a list of named files.
 package main
 
 import (
-	"fmt"
+	"bufio"
 	"os"
+
+	"github.com/zyc737347123/goPractice/base/funcs"
 )
 
+// LineInfo is a struct for record text line info
+type LineInfo struct {
+	name string
+	text string
+}
+
 func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
+	files := os.Args[1:]
+	funcs.CountFilesLines(files)
+}
+
+func countLines(f *os.File, counts map[LineInfo]int, fileName string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		lineInfo := LineInfo{name: fileName, text: input.Text()}
+		counts[lineInfo]++
 	}
-	fmt.Println(s)
+	// NOTE: ignoring potential errors from input.Err()
 }
