d1dd52db47a7352e4a351c52f4f085ff552ab701
linkz <zyc737347123@163.com>
2020-01-16 19:51:47 +0800

    change visit func

--- base/crawler/func.go
+++ base/crawler/func.go
@@ -18,6 +18,17 @@ func Visit(file *os.File) {
 	}
 }
 
+func VisitR(file *os.File) {
+	doc, err := html.Parse(file)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visitR(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
 // visit appends to links each link found in n and returns the result.
 func visit(links []string, n *html.Node) []string {
 	if n.Type == html.ElementNode && n.Data == "a" {
