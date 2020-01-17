dc08a40c04aa9aa24c129a79994b544da0231d6a
linkz <zyc737347123@163.com>
2020-01-17 12:54:12 +0800

    add new func

--- base/crawler/func.go
+++ base/crawler/func.go
@@ -7,6 +7,7 @@ import (
 	"golang.org/x/net/html"
 )
 
+// Visit is func
 func Visit(file *os.File) {
 	doc, err := html.Parse(file)
 	if err != nil {
@@ -18,6 +19,7 @@ func Visit(file *os.File) {
 	}
 }
 
+// VisitR is func
 func VisitR(file *os.File) {
 	doc, err := html.Parse(file)
 	if err != nil {
@@ -36,6 +38,7 @@ func visit(links []string, n *html.Node) []string {
 			if a.Key == "href" {
 				links = append(links, a.Val)
 			}
+			fmt.Println(a)
 		}
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
@@ -57,12 +60,58 @@ func visitR(links []string, n *html.Node) []string {
 			}
 		}
 	}
-	// for c := n.FirstChild; c != nil; c = c.NextSibling {
-	// 	links = visit(links, c)
-	// }
 
 	links = visitR(links, n.FirstChild)
 	links = visitR(links, n.NextSibling)
 
 	return links
+}
+
+func count(res map[string]int, n *html.Node) map[string]int {
+	if n.Type == html.ElementNode {
+		res[n.Data]++
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		res = count(res, c)
+	}
+	return res
+}
+
+// Count is a func
+func Count(file *os.File) {
+	doc, err := html.Parse(file)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for k, v := range count(nil, doc) {
+		fmt.Println(k, v)
+	}
+}
+
+func visit3(texts []string, n *html.Node) []string {
+
+	if n.Data == "script" || n.Data == "style" {
+		return texts
+	}
+
+	if n.Type == html.TextNode {
+		texts = append(texts, n.Data)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		texts = visit3(texts, c)
+	}
+	return texts
+}
+
+// Visit3 is a func
+func Visit3(file *os.File) {
+	doc, err := html.Parse(file)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for k, v := range visit3(nil, doc) {
+		fmt.Println(k, v)
+	}
 }
