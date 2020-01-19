46c8578c1cf67db849be07b6e884862bf8cbe8c0
linkz <zyc737347123@163.com>
2020-01-19 16:53:10 +0800

    add retry func

---
+++ base/crawler/retry.go
@@ -0,0 +1,25 @@
+package crawler
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+// WaitForServer attempts to contact the server of a URL.
+// It tries for one minute using exponential back-off.
+// It reports an error if all attempts fail.
+func WaitForServer(url string) error {
+	const timeout = 1 * time.Minute
+	deadline := time.Now().Add(timeout)
+	for tries := 0; time.Now().Before(deadline); tries++ {
+		_, err := http.Head(url)
+		if err == nil {
+			return nil // success
+		}
+		log.Printf("server not responding (%s);retrying…", err)
+		time.Sleep(time.Second << uint(tries)) // exponential back-off
+	}
+	return fmt.Errorf("server %s failed to respond after %s", url, timeout)
+}
--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/zyc737347123/goPractice/base/crawler"
 )
 
 func main() {
-	fmt.Println(crawler.CountWordsAndImages(os.Args[1]))
+	log.SetPrefix("wait: ")
+	log.SetFlags(0)
+	if err := crawler.WaitForServer(os.Args[1]); err != nil {
+		fmt.Fprintf(os.Stderr, "Site is down: %v\n", err)
+		log.Fatalf("Site is down: %v\n", err)
+		os.Exit(1)
+	}
 }
