4e257eab4afb5a3c78debdedba261fc853717bef
zyc737347123 <zyc737347123@163.com>
2020-02-04 17:59:37 +0800

    update go 5.6

--- base/crawler/findlink.go
+++ base/crawler/findlink.go
@@ -43,3 +43,41 @@ func FindLinks2(url string) (*html.Node, error) {
 	}
 	return doc, nil
 }
+
+// Extract makes an http get request to the specified url, parses
+func Extract(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	var links []string
+	visitNode := func(n *html.Node) bool {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue
+				}
+				link, err := resp.Request.URL.Parse(a.Val)
+				if err != nil {
+					continue
+				}
+				links = append(links, link.String())
+			}
+		}
+		return true
+	}
+	ForEachNode(doc, visitNode, nil)
+	return links, nil
+}
---
+++ base/funcs/bfs.go
@@ -0,0 +1,17 @@
+package funcs
+
+// BFS is breadth First serach for each item in worklist
+// f is called at most once for each item
+func BFS(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
